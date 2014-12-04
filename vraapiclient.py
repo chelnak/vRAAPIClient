3eb5df65595bc93eae0f95ce108e16a0d26b573b
chelnak <craiggumbey@gmail.com>
2014-12-04 17:04:53 +0000

    Many improvements and added new functions

--- vraapiclient.py
+++ vraapiclient.py
@@ -3,38 +3,59 @@
 from __future__ import unicode_literals
 import requests, json
 import sys
+__author__ = 'https://github.com/chelnak'
+
+
 
 def checkResponse(r):
-	#Quick logic to check the http response code
-	if r.status_code != 200:
+	"""
+	Quick logic to check the http response code.
+	
+	Parameters:
+		r = http response object.
+	"""
+
+	acceptedResponses = [200,201]
+	if not r.status_code in acceptedResponses:
 		print "STATUS: {status} ".format(status=r.status_code)
 		print "ERROR: " + r.text
 		sys.exit(r.status_code)
 
 def authenticate(host, user, password, tenant):
-	#Build POST for authentication
+	"""
+	Function that will authenticate a user and build.
+
+	Parameters:
+		host = vRA Appliance fqdn.
+		user = user account with access to the vRA portal.
+		passowrd = valid password for above user.
+		tenant = tenant for the user.
+	"""
+
 	headers = { 'Content-Type' : 'application/json', 'Accept' : 'application/json'}
-
 	payload = {"username": user, "password": password, "tenant": tenant}
-
 	url = 'https://'+ host + '/identity/api/tokens'
+	r = requests.post(url = url, data = json.dumps(payload), headers = headers, verify = False)
+	checkResponse(r)
+	response =  r.json() 
 
-	#Send Request
-	req = requests.post(url = url, data = json.dumps(payload), headers = headers, verify = False)
-
-	#Check request response is 200
-	checkResponse(req)
-
-	#Get JSON response
-	response =  req.json() 
-
-	#Build bearer token
 	usr_token = 'Bearer ' + response[u'id']
 
 	return usr_token
 
-class vRAAPIClient(object):
+class vRAAPIConsumerClient(object):
 	def __init__(self, host, username, password, tenant=None):
+		"""
+		Creates a connection to the vRA REST API using the provided
+		username and password.
+
+		Parameters:
+	                host = vRA Appliance fqdn
+        	        user = user account with access to the vRA portal
+                	passowrd = valid password for above user
+	                tenant = tenant for user. if this is NONE it will default to "vsphere.local"
+		"""
+
 		if tenant is None:
 			tenant = "vsphere.local"
 
@@ -45,33 +66,56 @@ class vRAAPIClient(object):
 		self.token = authenticate(host, username, password, tenant)
 
 	def getToken(self):
+		"""
+		Function that prints the bearer token for the session.
+		This is only for troubleshooting.
+		"""
+
 		print self.token
 
 	def getResource(self, id):
+		"""
+		Function that will get a vRA resource by id.
+
+		Parameters:
+			id = id of the vRA resource.
+		"""
+
 		host = self.host
 		token = self.token
 		
 		url = 'https://' + host + '/catalog-service/api/consumer/resources/' + id
-		headers = { 'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
-		req = requests.get(url = url, headers = headers, verify = False)
-		checkResponse(req)
-		resource = req.json()
+		headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
+		r = requests.get(url = url, headers = headers, verify = False)
+		checkResponse(r)
+		resource = r.json()
 	
 		return resource
 
 	def getAllResources(self):
+		"""
+		Function that will return all resources that are available to the current user.
+		"""
+
 		host = self.host
 		token = self.token
 
 		url = 'https://' + host + '/catalog-service/api/consumer/resources'
-		headers = { 'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
-		req = requests.get(url = url, headers = headers, verify = False)
-		checkResponse(req)
-		resources = req.json()
+		headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
+		r = requests.get(url = url, headers = headers, verify = False)
+		checkResponse(r)
+		resources = r.json()
 
 		return resources[u'content']
 	
 	def getResourceNetworking(self, id):
+		"""
+		Function that will return networking information for a given resource.
+
+		Parameters:
+			id = id of the vRA resource. 
+		"""
+
                 host = self.host
                 token = self.token
 	
@@ -84,3 +128,43 @@ class vRAAPIClient(object):
 				for j in networkList:
 					entries = j['values']['entries']
 		return entries
+	
+	def getRequest(self, id):
+		"""
+		Function that will return request information for a given request.
+
+		Parameters:
+			id = the id of the vRA request.
+		"""
+
+	        host = self.host
+                token = self.token
+
+                url = 'https://' + host + '/catalog-service/api/consumer/requests/' + id
+                headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
+                r = requests.get(url = url, headers = headers, verify = False)
+		checkResponse(r)
+
+		return r.json()
+
+	def requestResource(self, payload):
+		"""
+		Function that will submit a request based on payload.
+		payload = json body (example in request.json)
+
+		Parameters:
+			payload = JSON request body.
+		"""	
+	
+                host = self.host
+                token = self.token
+		
+                url = 'https://' + host + '/catalog-service/api/consumer/requests'
+                headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json', 'Authorization' : token}
+	        r = requests.post(url = url, data = json.dumps(payload), headers = headers, verify = False)
+		checkResponse(r)
+
+		id = r.headers['location'].split('/')[7]
+		
+		return id
+
