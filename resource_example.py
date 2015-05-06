c138a86e52392daa282123b36a30be0765f55256
chelnak <craiggumbey@gmail.com>
2015-05-06 14:01:16 +0100

    Reservation service client

---
+++ resource_example.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from vraapiclient import vRAAPIConsumerClient
+import json
+from time import sleep
+import sys
+import getpass
+
+url = "vra.org.com"
+usr = "user"
+passwd = getpass.getpass()
+
+#Create a new consumer API client
+client = vRAAPIConsumerClient(url, usr, passwd)
+
+#requests.json file must exist and have valid JSON content
+with open('templates/request.json') as f:
+	payload = json.load(f)
+
+#Submit new request. requestResource returns the request id
+request = client.requestResource(payload)
+
+#Monitor request progress and break from loop when no longer IN_PROGRESS
+while True:
+	sleep(10)
+	
+	requestState = client.getRequest(request)
+	print requestState['state']
+	if requestState['state'] != "IN_PROGRESS":
+		break
+
+#Extra logic to determine the result of the request
+if requestState['state'] != "SUCCESSFUL":
+	exit(requestState)
+
+#End goal here is to return networking info of the new vm so we can grab the IP and run any 
+#extra tasks etc
+resourceId = client.getResourceId(request)
+resourceNetworking = client.getResourceNetworking(resourceId)
+print resourceNetworking
