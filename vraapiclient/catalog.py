d2d1ae0ec9eb35ce2b4660321a8268845f3ab048
Craig Gumbley <craiggumbley@gmail.com>
2015-08-06 14:32:54 +0100

    Indent

--- vraapiclient/catalog.py
+++ vraapiclient/catalog.py
@@ -66,7 +66,7 @@ class ConsumerClient(object):
                 resource['status'],
                 resource['catalogItem']['label']])
 
-                print table
+            print table
 
         elif show =='json':
             return resource
