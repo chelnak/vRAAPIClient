909dc5f14db2c99fd0a677b94d110b2f133eb7e1
Craig Gumbley <craiggumbley@gmail.com>
2015-08-11 16:16:54 +0100

    Strange doc issues

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='vRAAPIClient',
-      version='1.0.2',
+      version='1.0.2.1',
       description='vRealize Automation API Client',
       author='chelnak',
       author_email='',
