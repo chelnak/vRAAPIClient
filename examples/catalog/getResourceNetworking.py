30744f516cf54dedc0d98e73c30abbf2263ccb5b
Tomáš Kuba <tomas.kuba@ysoft.com>
2015-08-14 16:58:42 +0200

    getResourceNetworking example now returns networking data

--- examples/catalog/getResourceNetworking.py
+++ examples/catalog/getResourceNetworking.py
@@ -10,12 +10,15 @@ client = catalog.ConsumerClient(url, usr, passwd)
 
 resourceId='171d8ab9-1b5b-44e8-ac20-b559da4c1ef3'
 
-#Get JSON Object
-resource = client.getResource(id=resourceId)
+#Print table
+resource = client.getResourceNetworking(id=resourceId, show='table')
 
-#Print id and name
-print resource['id']
-print resource['name']
+#Get JSON Object
+resource = client.getResourceNetworking(id=resourceId)
+
+#Print networking values
+for i in resource:
+    print([i['key'], i['value']['value']])
 
 #Use json.dumps() to get raw json
 resourceRaw = json.dumps(resource)
